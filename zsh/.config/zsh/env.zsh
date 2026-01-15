5e1f0388c84720c00b0a74668892bc4ef2eedbf7
Danny <danny@DannynoMacBook-Pro.local>
2026-01-15 09:03:42 +0900

    feat: vim add - nvim, pnpm & rbenv path

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -115,3 +115,34 @@ fi
 if command -v starship &> /dev/null; then
   eval "$(starship init zsh)"
 fi
+
+# ==============================================================================
+# Added by LM Studio CLI (lms)
+# ==============================================================================#
+
+export PATH="$PATH:/Users/danny/.lmstudio/bin"
+
+# ==============================================================================
+# Nvm
+# ==============================================================================
+
+export NVM_DIR="$HOME/.nvm"
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# ==============================================================================
+# Pnpm
+# ==============================================================================
+
+export PNPM_HOME="/Users/danny/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+# ==============================================================================
+# Ruby - Rbenv
+# ==============================================================================
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -10,7 +10,3 @@
 source ~/.config/zsh/env.zsh
 source ~/.config/zsh/aliases.zsh
 source ~/.config/zsh/plugins.zsh
-
-# Added by LM Studio CLI (lms)
-export PATH="$PATH:/Users/danny/.lmstudio/bin"
-# End of LM Studio CLI section
